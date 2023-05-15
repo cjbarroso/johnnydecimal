d0fa9d4cb719d05068edb65f3e95d2814b4bb630
Carlos Barroso <carlos@cjbarroso.com>
2023-05-15 09:46:29 -0300

    corrected missing quotes

--- jncreate.sh
+++ jncreate.sh
@@ -2,13 +2,13 @@
 # Create a new JD number in the provided location
 
 PARENTDIR=$1
-if [ -z $PARENTDIR ]; then
+if [ -z "$PARENTDIR" ]; then
   echo "ERROR: Base directory not provided"
   exit 1
 fi 
 
 # First we find the last directory in directory
-LAST=$(ls -1 $PARENTDIR | sort | tail -n1 | awk '{print $1}')
+LAST=$(ls -1 "$PARENTDIR" | sort | tail -n1 | awk '{print $1}')
 
 # Now we separate the AREA part from the ID part
 readarray -d . -t strarr <<< "$LAST"
